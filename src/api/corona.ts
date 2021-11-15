2170f7dd59ef8e90e64a9e17b04615d0ac22385c
tjdanqkr <tjdanqkr@gmail.com>
2021-11-16 03:03:34 +0900

    api

--- src/api/corona.ts
+++ src/api/corona.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://www.tjdanqkr.kro.kr/';
+axios.defaults.baseURL = 'https://www.tjdanqkr.kro.kr';
 
 const coronaApi = async (start = 1000) => {
   const res = await axios.get(`/corona/${start}/`);
