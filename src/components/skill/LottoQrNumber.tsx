db466a48fbb4e4f1efeba1262918fd6585e51b30
tjdanqkr <tjdanqkr@gmail.com>
2022-01-10 18:28:58 +0900

    qr

--- src/components/skill/LottoQrNumber.tsx
+++ src/components/skill/LottoQrNumber.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import QrReader from 'react-qr-reader';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 
@@ -7,14 +7,18 @@ function LottoQrNumber() {
   const statistics = useAppSelector((state) => state.lotto.statistics);
   const suggestion = useAppSelector((state) => state.lotto.suggestion);
   const dispatch = useAppDispatch();
+  const [da, setData] = useState();
+  const [er, setError] = useState();
 
   const handleScan = (data: any) => {
     if (data) {
       console.log(data);
+      setData(data);
     }
   };
   const handleError = (err: any) => {
     console.error(err);
+    setError(err);
   };
 
   return (
@@ -26,6 +30,9 @@ function LottoQrNumber() {
           onScan={handleScan}
           style={{ width: '100%' }}
         />
+
+        <p>data: {da}</p>
+        <p>err: {er}</p>
       </div>
     </>
   );
